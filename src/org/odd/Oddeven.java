bfe83659171d6bdfbef1ca351b1f110db53d53a8
VijayMR <vijayragupathy23@gmail.com>
2021-08-06 11:09:49 +0530

    track

---
+++ src/org/odd/Oddeven.java
@@ -0,0 +1,20 @@
+package org.odd;
+
+import java.util.Scanner;
+
+public class Oddeven {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("please enter the value");
+		int a = s.nextInt();
+		
+		if(a%2 == 0) {
+			System.out.println("This is even number");
+		}
+		else {
+			System.out.println("This is odd number");
+		}
+	}
+
+}
